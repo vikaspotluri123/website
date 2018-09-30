115faf6faa8294a29481053e58371df0eb329dc5
Vikas <potlurivikas@gmail.com>
2018-09-29 22:39:14 -0500

    Create pages for each concept
     - all pages are the same, there's just a different section that's
     initially active

--- tasks/build-html.js
+++ tasks/build-html.js
@@ -1,9 +1,44 @@
 const gulp = require('gulp');
 const htmlreplace = require('gulp-html-replace');
 const htmlmin = require('gulp-htmlmin');
+const {readFile, writeFile} = require('fs');
+const {promisify} = require('util');
 
-module.exports.dev = () => gulp.src('./src/*.html').pipe(gulp.dest('./build'));
-module.exports.prod = () => {
+const read = promisify(readFile);
+const write = promisify(writeFile);
+
+const PAGES = ['index', 'motivation', 'experience', 'resume'];
+
+function makePages() {
+	return read('./build/index.html', 'utf8').then(index =>
+		PAGES.reduce((files, page) => {files[page] = index; return files}, {})
+	).then(files => {
+		const promises = [];
+		const pages = PAGES.map(page => ({
+			exp: new RegExp(` ACTIVE_IF_${page.toUpperCase()}`, 'g'),
+			page
+		}));
+
+		Object.keys(files).forEach(fileName => {
+			let file = files[fileName];
+			pages.forEach(({page, exp}) => {
+				file = file.replace(exp, fileName === page ? ' active' : '');
+			});
+
+			file = file.replace('ACTIVE_PAGE', `active-${fileName.replace('index', 'introduction')}`);
+			promises.push(write(`./build/${fileName}.html`, file));
+		});
+
+		return Promise.all(promises);
+	}).then(() => null);
+}
+
+module.exports.dev = cb => {
+	gulp.src('./src/*.html')
+		.pipe(gulp.dest('./build'))
+		.on('end', () => makePages().then(cb));
+}
+module.exports.prod = cb => {
 const assets = Object.values(require('../build/rev-manifest.json'));
 
 	const css = assets.filter(asset => asset.toLowerCase().endsWith('css'))
@@ -13,7 +48,7 @@ const assets = Object.values(require('../build/rev-manifest.json'));
 
 	const js = ['./assets/js/load-service-worker.js', ...jsAssets];
 
-	return gulp.src('./src/*.html')
+	gulp.src('./src/*.html')
 		.pipe(htmlreplace({
 			css: {
 				src: css,
@@ -28,5 +63,8 @@ const assets = Object.values(require('../build/rev-manifest.json'));
 			collapseWhitespace: true,
 			removeComments: true
 		}))
-		.pipe(gulp.dest('build'));
+		.pipe(gulp.dest('build'))
+		.on('end', () => {
+			makePages.then(cb);
+		});
 };
