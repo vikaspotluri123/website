ce76ce5b13dd28f3d7722bc6efffdddb1adcf86a
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-07-12 12:24:56 -0500

    spaces -> tabs

--- blog-theme/gulpfile.js
+++ blog-theme/gulpfile.js
@@ -15,65 +15,65 @@ var cssnano = require('cssnano');
 var easyimport = require('postcss-easy-import');
 
 function serve(done) {
-    livereload.listen();
-    done();
+	livereload.listen();
+	done();
 }
 
 const handleError = (done) => {
-    return function (err) {
-        if (err) {
-            beeper();
-        }
-        return done(err);
-    };
+	return function (err) {
+		if (err) {
+			beeper();
+		}
+		return done(err);
+	};
 };
 
 function hbs(done) {
-    pump([
-        src(['*.hbs', '**/**/*.hbs', '!node_modules/**/*.hbs']),
-        livereload()
-    ], handleError(done));
+	pump([
+		src(['*.hbs', '**/**/*.hbs', '!node_modules/**/*.hbs']),
+		livereload()
+	], handleError(done));
 }
 
 function css(done) {
-    var processors = [
-        easyimport,
-        colorFunction(),
-        autoprefixer(),
-        cssnano()
-    ];
+	var processors = [
+		easyimport,
+		colorFunction(),
+		autoprefixer(),
+		cssnano()
+	];
 
-    pump([
-        src('assets/css/*.css', {sourcemaps: true}),
-        postcss(processors),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
+	pump([
+		src('assets/css/*.css', {sourcemaps: true}),
+		postcss(processors),
+		dest('assets/built/', {sourcemaps: '.'}),
+		livereload()
+	], handleError(done));
 }
 
 function js(done) {
-    pump([
-        src('assets/js/*.js', {sourcemaps: true}),
-        uglify(),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
+	pump([
+		src('assets/js/*.js', {sourcemaps: true}),
+		uglify(),
+		dest('assets/built/', {sourcemaps: '.'}),
+		livereload()
+	], handleError(done));
 }
 
 function zipper(done) {
-    var targetDir = 'dist/';
-    var themeName = require('./package.json').name;
-    var filename = themeName + '.zip';
+	var targetDir = 'dist/';
+	var themeName = require('./package.json').name;
+	var filename = themeName + '.zip';
 
-    pump([
-        src([
-            '**',
-            '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**'
-        ]),
-        zip(filename),
-        dest(targetDir)
-    ], handleError(done));
+	pump([
+		src([
+			'**',
+			'!node_modules', '!node_modules/**',
+			'!dist', '!dist/**'
+		]),
+		zip(filename),
+		dest(targetDir)
+	], handleError(done));
 }
 
 const cssWatcher = () => watch('assets/css/**', css);
