8519c7a52c19a63dc8f38dfb0db59e5ad056a87f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-07-14 00:36:34 -0500

    remove css and js processing from blog gulpfile

--- blog-theme/gulpfile.js
+++ blog-theme/gulpfile.js
@@ -1,18 +1,10 @@
-const {series, watch, src, dest, parallel} = require('gulp');
+const {series, watch, src, dest} = require('gulp');
 const pump = require('pump');
 
 // gulp plugins and utils
 var livereload = require('gulp-livereload');
-var postcss = require('gulp-postcss');
 var zip = require('gulp-zip');
-var uglify = require('gulp-uglify');
 var beeper = require('beeper');
-
-// postcss plugins
-var autoprefixer = require('autoprefixer');
-var colorFunction = require('postcss-color-mod-function');
-var cssnano = require('cssnano');
-var easyimport = require('postcss-easy-import');
 
 function serve(done) {
 	livereload.listen();
@@ -35,31 +27,6 @@ function hbs(done) {
 	], handleError(done));
 }
 
-function css(done) {
-	var processors = [
-		easyimport,
-		colorFunction(),
-		autoprefixer(),
-		cssnano()
-	];
-
-	pump([
-		src('assets/css/*.css', {sourcemaps: true}),
-		postcss(processors),
-		dest('assets/built/', {sourcemaps: '.'}),
-		livereload()
-	], handleError(done));
-}
-
-function js(done) {
-	pump([
-		src('assets/js/*.js', {sourcemaps: true}),
-		uglify(),
-		dest('assets/built/', {sourcemaps: '.'}),
-		livereload()
-	], handleError(done));
-}
-
 function zipper(done) {
 	var targetDir = 'dist/';
 	var themeName = require('./package.json').name;
@@ -76,12 +43,9 @@ function zipper(done) {
 	], handleError(done));
 }
 
-const cssWatcher = () => watch('assets/css/**', css);
 const hbsWatcher = () => watch(['*.hbs', '**/**/*.hbs', '!node_modules/**/*.hbs'], hbs);
-const watcher = parallel(cssWatcher, hbsWatcher);
-const build = series(css, js);
-const dev = series(build, serve, watcher);
+const watcher = hbsWatcher;
+const dev = series(serve, watcher);
 
-exports.build = build;
-exports.zip = series(build, zipper);
+exports.zip = series(zipper);
 exports.default = dev;
