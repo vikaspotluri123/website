895a0886fb997543cb203fdbee59734008fab1b4
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-07-14 12:56:17 -0500

    possibly add support for purging unused css in blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	purge: [
+		'./src/assets/blog/**/*.hbs',
 		'./dist/**/*.html'
 	],
 	theme: {
 		container: {
 			center: true
 		},
-	extend: {
+		extend: {
 			colors: {
 				white: '#ebebeb',
 				black: '#4F4F4F'
